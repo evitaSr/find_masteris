1960de0b5ef718a70c7d26492c6d38d174254421
Evita <evita.sriupsaite@gmail.com>
2025-09-18 19:11:13 +0300

    add db

--- findMasteris/settings.py
+++ findMasteris/settings.py
@@ -12,6 +12,8 @@ https://docs.djangoproject.com/en/5.2/ref/settings/
 
 from pathlib import Path
 
+import settings_db
+
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
 
@@ -75,12 +77,7 @@ WSGI_APPLICATION = "findMasteris.wsgi.application"
 # Database
 # https://docs.djangoproject.com/en/5.2/ref/settings/#databases
 
-DATABASES = {
-    "default": {
-        "ENGINE": "django.db.backends.sqlite3",
-        "NAME": BASE_DIR / "db.sqlite3",
-    }
-}
+DATABASES = settings_db.DATABASES
 
 
 # Password validation
