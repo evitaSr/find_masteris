4a28ef9d9e3987b40e1a88a48bd11048cf6d2bf6
Evita <evita.sriupsaite@gmail.com>
2025-11-23 15:53:50 +0200

    make password not required in user edit form

--- find-masteris-backend/api/serializers.py
+++ find-masteris-backend/api/serializers.py
@@ -34,7 +34,8 @@ class PasswordHashingSerializer(serializers.ModelSerializer):
         return super().create(validated_data)
 
     def update(self, instance, validated_data):
-        validated_data['password'] = make_password(validated_data['password'])
+        if 'password' in validated_data:
+            validated_data['password'] = make_password(validated_data['password'])
         return super().update(instance, validated_data)
 
 
