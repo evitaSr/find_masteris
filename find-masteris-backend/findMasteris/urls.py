413c8908aefc04654e6fca5b0d5ae3156b3f7d57
Evita <evita.sriupsaite@gmail.com>
2025-11-29 20:13:01 +0200

    fix base url file

--- find-masteris-backend/findMasteris/urls.py
+++ find-masteris-backend/findMasteris/urls.py
@@ -17,7 +17,4 @@ schema_view = get_schema_view(
 urlpatterns = [
     path('', include('api.urls')),
     path('swagger/', schema_view.with_ui('swagger', cache_timeout=0), name='schema-swagger'),
-]
-
-if settings.DEBUG:
-    urlpatterns += static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
+] + static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
